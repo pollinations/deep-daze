1fa95360f1503bb410c383f56185b7f942f310cc
Exocamp <scamgiver@gmail.com>
2021-06-18 17:35:40 -0500

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- deep_daze/moresiren.py
+++ deep_daze/moresiren.py
@@ -61,7 +61,39 @@ class CustomSirenNet(nn.Module):
 
         return self.last_layer(x)
 
-class CustomSirenWrapper(SirenWrapper):
+class CustomSirenWrapper(nn.Module):
     def __init__(self, net, image_width, image_height, latent_dim = None):
         super().__init__()
         assert isinstance(net, CustomSirenNet), 'CustomSirenWrapper must receive a custom Siren network'
+
+        self.net = net
+        self.image_width = image_width
+        self.image_height = image_height
+
+        self.modulator = None
+        if exists(latent_dim):
+            self.modulator = Modulator(
+                dim_in = latent_dim,
+                dim_hidden = net.dim_hidden,
+                num_layers = net.num_layers
+            )
+
+        tensors = [torch.linspace(-1, 1, steps = image_width), torch.linspace(-1, 1, steps = image_height)]
+        mgrid = torch.stack(torch.meshgrid(*tensors), dim=-1)
+        mgrid = rearrange(mgrid, 'h w c -> (h w) c')
+        self.register_buffer('grid', mgrid)
+
+    def forward(self, img = None, *, latent = None):
+        modulate = exists(self.modulator)
+        assert not (modulate ^ exists(latent)), 'latent vector must be only supplied if `latent_dim` was passed in on instantiation'
+
+        mods = self.modulator(latent) if modulate else None
+
+        coords = self.grid.clone().detach().requires_grad_()
+        out = self.net(coords, mods)
+        out = rearrange(out, '(h w) c -> () c h w', h = self.image_height, w = self.image_width)
+
+        if exists(img):
+            return F.mse_loss(img, out)
+
+        return out
