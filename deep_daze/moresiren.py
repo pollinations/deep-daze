73f2af5b7b14f32d44dd87dc4b6c0601db547fe8
Exocamp <scamgiver@gmail.com>
2021-06-18 18:25:57 -0500

    Is this an end to the pain?

--- deep_daze/moresiren.py
+++ deep_daze/moresiren.py
@@ -25,6 +25,36 @@ class CustomActivation(nn.Module):
     def forward(self, x):
         return self.activation(self.w0 * x)
 
+#aight I guess I have to just import the whole Siren module. okay then.
+
+class CustomSiren(nn.Module):
+    def __init__(self, dim_in, dim_out, w0 = 1., c = 6., is_first = False, use_bias = True, layer_activation=torch.sin, final_activation = None):
+        super().__init__()
+        self.dim_in = dim_in
+        self.is_first = is_first
+
+        weight = torch.zeros(dim_out, dim_in)
+        bias = torch.zeros(dim_out) if use_bias else None
+        self.init_(weight, bias, c = c, w0 = w0)
+
+        self.weight = nn.Parameter(weight)
+        self.bias = nn.Parameter(bias) if use_bias else None
+        self.activation = CustomActivation(torch_activation=layer_activation, w0=w0) if final_activation is None else final_activation
+
+    def init_(self, weight, bias, c, w0):
+        dim = self.dim_in
+
+        w_std = (1 / dim) if self.is_first else (math.sqrt(c / dim) / w0)
+        weight.uniform_(-w_std, w_std)
+
+        if exists(bias):
+            bias.uniform_(-w_std, w_std)
+
+    def forward(self, x):
+        out =  F.linear(x, self.weight, self.bias)
+        out = self.activation(out)
+        return out
+
 #because I don't wanna do 2 repos, here's a more "open" SirenNet class, and by that I mean just changing activations on the layers themselves lol
 class CustomSirenNet(nn.Module):
     def __init__(self, dim_in, dim_hidden, dim_out, num_layers, w0 = 1., w0_initial = 30., use_bias = True, layer_activation = None, final_activation = None):
@@ -38,7 +68,7 @@ class CustomSirenNet(nn.Module):
             layer_w0 = w0_initial if is_first else w0
             layer_dim_in = dim_in if is_first else dim_hidden
 
-            self.layers.append(Siren(
+            self.layers.append(CustomSiren(
                 dim_in = layer_dim_in,
                 dim_out = dim_hidden,
                 w0 = layer_w0,
@@ -60,6 +90,7 @@ class CustomSirenNet(nn.Module):
                 x *= rearrange(mod, 'd -> () d')
 
         return self.last_layer(x)
+
 
 class CustomSirenWrapper(nn.Module):
     def __init__(self, net, image_width, image_height, latent_dim = None):
