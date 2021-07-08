403dc0161169f510c8f227cb4d85b147e8c17da0
Thomas Haferlach <t.haferlach@gmail.com>
2021-07-08 17:06:36 +0200

    Update multiplicative.py

--- deep_daze/multiplicative.py
+++ deep_daze/multiplicative.py
@@ -51,9 +51,10 @@ class MFN(nn.Module):
 
         coords = self.grid.clone().detach().requires_grad_()
         out = self.model(coords)
-        # out = (out*0.5 + 0.5).clamp(0,1)
+        print("pre shape",out.shape)
+        out = (out*0.5 + 0.5).clamp(0,1)
         out = rearrange(out, '(h w) c -> () c h w', h = self.image_height, w = self.image_width)
-
+        print("post shape",out.shape)
         if exists(img):
             return F.mse_loss(img, out)
 
